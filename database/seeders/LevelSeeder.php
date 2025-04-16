a9a8b16615d51e8e3462c699a1cd368052f76d76
Yeon <giojio825@gmail.com>
2025-04-16 15:44:19 +0700

    Add timestamps to user and level seeders for created_at and updated_at fields

--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -14,11 +14,11 @@ class LevelSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['level_id' => 1, 'level_kode' => 'ADM', 'level_nama' => 'Administrator'],
-            ['level_id' => 2, 'level_kode' => 'MNG', 'level_nama' => 'Manager'],
-            ['level_id' => 3, 'level_kode' => 'STF', 'level_nama' => 'Staff/Kasir'],
-            ['level_id' => 4, 'level_kode' => 'SUP', 'level_nama' => 'Supplier'],
-            ['level_id' => 5, 'level_kode' => 'PLG', 'level_nama' => 'Pelanggan']
+            ['level_id' => 1, 'level_kode' => 'ADM', 'level_nama' => 'Administrator', 'created_at' => now()->setTimezone('Asia/Jakarta'), 'updated_at' => now()->setTimezone('Asia/Jakarta')],
+            ['level_id' => 2, 'level_kode' => 'MNG', 'level_nama' => 'Manager', 'created_at' => now()->setTimezone('Asia/Jakarta'), 'updated_at' => now()->setTimezone('Asia/Jakarta')],
+            ['level_id' => 3, 'level_kode' => 'STF', 'level_nama' => 'Staff/Kasir', 'created_at' => now()->setTimezone('Asia/Jakarta'), 'updated_at' => now()->setTimezone('Asia/Jakarta')],
+            ['level_id' => 4, 'level_kode' => 'SUP', 'level_nama' => 'Supplier', 'created_at' => now()->setTimezone('Asia/Jakarta'), 'updated_at' => now()->setTimezone('Asia/Jakarta')],
+            ['level_id' => 5, 'level_kode' => 'PLG', 'level_nama' => 'Pelanggan', 'created_at' => now()->setTimezone('Asia/Jakarta'), 'updated_at' => now()->setTimezone('Asia/Jakarta')]
         ];
 
         DB::table('m_level')->insert($data);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,48 +20,62 @@ class UserSeeder extends Seeder
                 'level_id'=> 1,
                 'username' => 'admin',
                 'nama' => 'Administrator',
-                'password' => Hash::make('123456')
+                'password' => Hash::make('123456'),
+                'created_at' => now()->setTimezone('Asia/Jakarta'),
+                'updated_at' => now()->setTimezone('Asia/Jakarta')
             ],
             [
                 'user_id' => 2,
                 'level_id'=> 2,
                 'username' => 'manager',
                 'nama' => 'Manager',
-                'password' => Hash::make('123456')
+                'password' => Hash::make('123456'),
+                'created_at' => now()->setTimezone('Asia/Jakarta'),
+                'updated_at' => now()->setTimezone('Asia/Jakarta')
             ],[
                 'user_id' => 3,
                 'level_id'=> 3,
                 'username' => 'staff',
                 'nama' => 'Staff/Kasir',
-                'password' => Hash::make('123456')
+                'password' => Hash::make('123456'),
+                'created_at' => now()->setTimezone('Asia/Jakarta'),
+                'updated_at' => now()->setTimezone('Asia/Jakarta')
             ],
             [
                 'user_id' => 4,
                 'level_id'=> 4,
                 'username' => 'makmur',
                 'nama' => 'Makmur Suprapdi',
-                'password' => Hash::make('123456')
+                'password' => Hash::make('123456'),
+                'created_at' => now()->setTimezone('Asia/Jakarta'),
+                'updated_at' => now()->setTimezone('Asia/Jakarta')
             ],
             [
                 'user_id' => 5,
                 'level_id'=> 4,
                 'username' => 'jaya',
                 'nama' => 'Jaya Wicaksana',
-                'password' => Hash::make('123456')
+                'password' => Hash::make('123456'),
+                'created_at' => now()->setTimezone('Asia/Jakarta'),
+                'updated_at' => now()->setTimezone('Asia/Jakarta')
             ],
             [
                 'user_id' => 6,
                 'level_id'=> 4,
                 'username' => 'abdi',
                 'nama' => 'King Abdi',
-                'password' => Hash::make('123456')
+                'password' => Hash::make('123456'),
+                'created_at' => now()->setTimezone('Asia/Jakarta'),
+                'updated_at' => now()->setTimezone('Asia/Jakarta')
             ],
             [
                 'user_id' => 7,
                 'level_id'=> 5,//pelanggan
                 'username' => 'budi',
                 'nama' => 'Budi Santoso',
-                'password' => Hash::make('123456')
+                'password' => Hash::make('123456'),
+                'created_at' => now()->setTimezone('Asia/Jakarta'),
+                'updated_at' => now()->setTimezone('Asia/Jakarta')
             ]
         ];
         DB::table('m_user')->insert($data);
